2a5022d3beb7bd41b8259c68b48bc1028de6d9ff
Adnan Gul <a.gul@nizek.com>
2025-09-27 09:19:53 +0500

    start of basic c concepts

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div  style={{textAlign:"left"}}>
+    <h1>React Basics</h1>
+     <div>
+
+     <h2>Jsx</h2>
+     <p>Jsx is use to add javascript expression inside markup.It use "{}" to escape back expressions.Its optional to use but make easier to work with react</p>
+     </div>
+     <div>
+
+     <h2>Thinking in react Steps</h2>
+     <h5 style={{textAlign:"left"}}>
+      1. Break the UI into a component hierarchy.<br/>
+      2. Build a static version in React.<br/>
+      3. Identify the minimal (but complete) representation of UI state.<br/>
+      4. Identify where your state should live.<br/>
+      5. Add inverse data flow.<br/>
+     </h5>
+     </div>
+     <div>
+
+     <h2>Installation</h2>
+     <h5 style={{textAlign:"left"}}>
+      From Scratch<br/>
+      Using nextjs<br/>
+      Using vite<br/>
+      Using Other react frameworks<br/>
+     </h5>
+     </div>
+    </div>
+  )
+}
+
+export default App
