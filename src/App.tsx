7a8cb14edf76fbe354ec3c89321527c42f0049ab
Adnan Gul <a.gul@nizek.com>
2025-09-27 09:59:47 +0500

    adding some basics

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,13 @@ function App() {
       Using Other react frameworks<br/>
      </h5>
      </div>
+     <div>
+
+     <h2>Describing the UI</h2>
+     <h5 style={{textAlign:"left"}}>
+     React is a JavaScript library for rendering user interfaces (UI). UI is built from small units like buttons, text, and images. React lets you combine them into reusable, nestable components. From web sites to phone apps, everything on the screen can be broken down into components. In this chapter, you’ll learn to create, customize, and conditionally display React components.
+     </h5>
+     </div>
     </div>
   )
 }
